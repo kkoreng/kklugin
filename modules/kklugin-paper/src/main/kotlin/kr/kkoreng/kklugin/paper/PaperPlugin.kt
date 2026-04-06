70fbfe1113aeb0e18e2d25a8981692c81456c3e1
kkoreng <chr54858@gmail.com>
2026-04-06 15:45:44 -0500

    chore: add paper module

---
+++ modules/kklugin-paper/src/main/kotlin/kr/kkoreng/kklugin/paper/PaperPlugin.kt
@@ -0,0 +1,4 @@
+package kr.kkoreng.kklugin.paper
+
+class PaperPlugin {
+}
---
+++ modules/kklugin-paper/src/main/kotlin/kr/kkoreng/kklugin/paper/tasks/GeneratePaperPluginYamlTask.kt
@@ -0,0 +1,11 @@
+package kr.kkoreng.kklugin.paper.tasks
+
+import kr.kkoreng.kklugin.core.GenerateMetadataTask
+
+abstract class GeneratePaperPluginYamlTask : GenerateMetadataTask() {
+
+
+    override fun generate() {
+        TODO("Not yet implemented")
+    }
+}
