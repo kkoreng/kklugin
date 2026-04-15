c31b065c18124b1704d6725675d2312a73f95e24
kkoreng <chr54858@gmail.com>
2026-04-15 18:13:49 -0500

    feat: BukkitKkluginExtension, PaperKkluginExtension 추가

---
+++ modules/kklugin-paper/src/main/kotlin/kr/kkoreng/kklugin/paper/extension/PaperKkluginExtension.kt
@@ -0,0 +1,14 @@
+package kr.kkoreng.kklugin.paper.extension
+
+import kr.kkoreng.kklugin.core.extension.KkluginExtension
+import kr.kkoreng.kklugin.paper.plugin.extension.PaperPluginExtension
+import org.gradle.api.Action
+import org.gradle.api.model.ObjectFactory
+import javax.inject.Inject
+
+    abstract class PaperKkluginExtension @Inject constructor(objects: ObjectFactory) : KkluginExtension(objects) {
+
+    val plugin: PaperPluginExtension = objects.newInstance(PaperPluginExtension::class.java)
+    fun plugin(action: Action<PaperPluginExtension>) = action.execute(plugin)
+
+}
