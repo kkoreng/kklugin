e11a97580fbb8a13359d77a902cd6529e7b90fe1
kkoreng <chr54858@gmail.com>
2026-04-15 15:34:50 -0500

    feat: plugin extension 내부 minecraftVersion property 추가

--- modules/kklugin-paper/src/main/kotlin/kr/kkoreng/kklugin/paper/plugin/extension/PaperPluginExtension.kt
+++ modules/kklugin-paper/src/main/kotlin/kr/kkoreng/kklugin/paper/plugin/extension/PaperPluginExtension.kt
@@ -15,6 +15,8 @@ import javax.inject.Inject
 
 abstract class PaperPluginExtension @Inject constructor(objects: ObjectFactory) {
 
+    @get:Input @get:Optional val minecraftVersion: Property<String> = objects.property(String::class.java)
+
     @get:Input val name: Property<String> = objects.property(String::class.java).convention("MyPlugin")
     @get:Input val version: Property<String> = objects.property(String::class.java).convention("1.0.0-SNAPSHOT")
     @get:Input val main: Property<String> = objects.property(String::class.java).convention("com.example.MyPlugin")
