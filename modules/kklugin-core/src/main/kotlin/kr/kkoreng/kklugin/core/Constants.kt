cd301b658aaf539b5465606771a34b2de30ed920
kkoreng <chr54858@gmail.com>
2026-04-15 18:15:05 -0500

    feat: Constants.kt 추가 - Constant Value 분리

---
+++ modules/kklugin-core/src/main/kotlin/kr/kkoreng/kklugin/core/Constants.kt
@@ -0,0 +1,9 @@
+package kr.kkoreng.kklugin.core
+
+object Constants {
+
+    object Paper {
+        const val REPO_URL = "https://repo.papermc.io/repository/maven-public/"
+        const val API_DEPENDENCY = "io.papermc.paper:paper-api:%s-R0.1-SNAPSHOT"
+    }
+}
