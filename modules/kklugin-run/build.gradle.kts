22426cbc5455c3d063c55ddb8d384df778318ab7
kkoreng <chr54858@gmail.com>
2026-04-15 18:20:15 -0500

    feat: kklugin-run 모듈 추가

---
+++ modules/kklugin-run/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("kr.kkoreng.kklugin")
+}
+
+/*
+gradlePlugin {
+    website.set("")
+    vcsUrl.set("")
+    plugins {
+        register("bukkit") {
+            id = "kr.kkoreng.kklugin.bukkit"
+            implementationClass = "kr.kkoreng.kklugin.bukkit.BukkitPlugin"
+        }
+    }
+}
+*/
