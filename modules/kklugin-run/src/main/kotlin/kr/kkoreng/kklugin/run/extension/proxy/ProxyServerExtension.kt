86c8177ff1dd04f939d65772c80a2ca43b2c95ad
kkoreng <chr54858@gmail.com>
2026-04-23 01:34:06 -0500

    feat: extension 클래스 수정: defaultServer 추가

--- modules/kklugin-run/src/main/kotlin/kr/kkoreng/kklugin/run/extension/proxy/ProxyServerExtension.kt
+++ modules/kklugin-run/src/main/kotlin/kr/kkoreng/kklugin/run/extension/proxy/ProxyServerExtension.kt
@@ -1,20 +1,24 @@
-package kr.kkoreng.kklugin.run.extension.proxy
+package com.kkoreng.kklugin.run.extension.proxy
 
-import kr.kkoreng.kklugin.run.enums.ProxyPlatform
-import kr.kkoreng.kklugin.run.extension.server.BackendServerExtension
-import kr.kkoreng.kklugin.run.extension.server.ServerBaseExtension
+import com.kkoreng.kklugin.run.enums.ProxyPlatform
+import com.kkoreng.kklugin.run.extension.server.BackendServerExtension
+import com.kkoreng.kklugin.run.extension.server.ServerBaseExtension
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.Optional
 import javax.inject.Inject
 
 abstract class ProxyServerExtension @Inject constructor(objects: ObjectFactory) : ServerBaseExtension(objects) {
 
     @get:Input val platform: Property<ProxyPlatform> = objects.property(ProxyPlatform::class.java)
 
-    val serverDirectory: Property<String> = objects.property(String::class.java).convention("run/proxy")
+    @get:Input val serverDirectory: Property<String> = objects.property(String::class.java).convention("run/proxy")
 
-    val backends: NamedDomainObjectContainer<BackendServerExtension> = objects.domainObjectContainer(BackendServerExtension::class.java)
+    @get:Input @get:Optional val defaultServer: Property<String> = objects.property(String::class.java)
+
+    @get:Nested val backends: NamedDomainObjectContainer<BackendServerExtension> = objects.domainObjectContainer(BackendServerExtension::class.java)
 
 }
